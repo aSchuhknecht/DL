45090d657000ad5d9d3556bbe5bb80b51390eb45
aSchuhknecht <adam.schuhknecht@gmail.com>
2023-07-01 00:28:03 -0400

    pushy hw3

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -94,7 +94,48 @@ class CNNClassifier(torch.nn.Module):
 
 
 class FCN(torch.nn.Module):
-    def __init__(self):
+
+    class DownBlock(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=(7, 7), padding=3, stride=(stride, stride)),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU()
+            )
+            self.downsample = None
+            if n_input != n_output or stride != 1:
+                self.downsample = torch.nn.Sequential(torch.nn.Conv2d(n_input, n_output, kernel_size=(1, 1), stride=(stride, stride)),
+                                                      torch.nn.BatchNorm2d(n_output))
+
+        def forward(self, x):
+            identity = x
+            if self.downsample is not None:
+                identity = self.downsample(identity)
+            return self.net(x) + identity
+            # return self.net(x)
+
+    class UpBlock(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=(7, 7), padding=(3, 3), stride =(stride, stride), output_padding=(1,1)),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU()
+            )
+            self.downsample = None
+            if n_input != n_output or stride != 1:
+                self.downsample = torch.nn.Sequential(torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=(1, 1), stride=(stride, stride), output_padding=(1,1)),
+                                                      torch.nn.BatchNorm2d(n_output))
+
+        def forward(self, x):
+            identity = x
+            if self.downsample is not None:
+                identity = self.downsample(identity)
+            return self.net(x) + identity
+            # return self.net(x)
+
+    def __init__(self, layers=None, n_input_channels=3):
         super().__init__()
         """
         Your code here.
@@ -104,7 +145,33 @@ class FCN(torch.nn.Module):
         Hint: Use residual connections
         Hint: Always pad by kernel_size / 2, use an odd kernel_size
         """
-        raise NotImplementedError('FCN.__init__')
+        if layers is None:
+            layers = [32, 64, 128]
+
+        down_layers = [32, 64, 128]
+        up_layers = [64, 32, 5]
+
+        L = []
+        c = 3
+        for lay in down_layers:
+            L.append(self.DownBlock(c, lay, stride=2))
+            c = lay
+        for lay in up_layers:
+            L.append(self.UpBlock(c, lay, stride=2))
+            c = lay
+
+        self.network = torch.nn.Sequential(*L)
+
+        self.downBlock1 = self.DownBlock(3, 32, stride=2)
+        self.downBlock2 = self.DownBlock(32, 64, stride=2)
+        self.downBlock3 = self.DownBlock(64, 128, stride=2)
+
+        self.UpBlock1 = self.UpBlock(128, 64, stride=2)
+        self.UpBlock2 = self.UpBlock(64*2, 32, stride=2)
+        self.UpBlock3 = self.UpBlock(32, 5, stride=2)
+        # self.classifier = torch.nn.Linear(c, 6)
+
+        # raise NotImplementedError('FCN.__init__')
 
     def forward(self, x):
         """
@@ -116,7 +183,30 @@ class FCN(torch.nn.Module):
               if required (use z = z[:, :, :H, :W], where H and W are the height and width of a corresponding strided
               convolution
         """
-        raise NotImplementedError('FCN.forward')
+        # z = self.network(x)
+
+        d1_in = x.size()
+        d1 = self.downBlock1(x)  # output of  first downConv
+
+        d2_in = d1.size()
+        d2 = self.downBlock2(d1)  # output of 2nd downConv
+
+        d3_in = d2.size()
+        d3 = self.downBlock3(d2)  # output of 3rd downConv
+
+        u1 = self.UpBlock1(d3)
+        u1 = u1[:, :, :d3_in[2], :d3_in[3]]
+        u1 = torch.cat((u1, d2), dim=1)  # skip connection
+
+        u2 = self.UpBlock2(u1)
+        u2 = u2[:, :, :d2_in[2], :d1_in[3]]
+        # u2 = torch.cat((u2, d1), dim=1)  # skip connection
+
+        u3 = self.UpBlock3(u2)
+        u3 = u3[:, :, :d1_in[2], :d1_in[3]]
+
+        return u3
+        # raise NotImplementedError('FCN.forward')
 
 
 model_factory = {
--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -1,15 +1,20 @@
 import torch
 import numpy as np
 
-from .models import FCN, save_model
-from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix
+from .models import FCN, save_model, ClassificationLoss
+from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix, accuracy
 from . import dense_transforms
 import torch.utils.tensorboard as tb
+from torchvision import transforms
 
 
 def train(args):
     from os import path
-    model = FCN()
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
+
+    model = FCN().to(device)
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
@@ -22,6 +27,70 @@ def train(args):
     Hint: If you found a good data augmentation parameters for the CNN, use them here too. Use dense_transforms
     Hint: Use the log function below to debug and visualize your model
     """
+
+    n_epochs = 100
+    batch_size = 128
+
+    trans = dense_transforms.Compose([
+        dense_transforms.ColorJitter(brightness=(0.5, 1.5), contrast=(1), saturation=(0.5, 1.5), hue=(-0.1, 0.1)),
+        dense_transforms.RandomHorizontalFlip(),
+        dense_transforms.ToTensor()
+    ])
+
+    train_dataloader = load_dense_data('dense_data/train', num_workers=0, batch_size=32, transform=trans)
+    valid_dataloader = load_dense_data('dense_data/valid')
+
+    #  optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
+    global_step = 0
+    acc = []
+    iou = []
+
+    for epoch in range(0, n_epochs):
+        for step, (data, labels) in enumerate(train_dataloader):
+            data, labels = data.to(device), labels.to(device)
+
+            output = model(data)
+            # print(labels.size())
+            # print(output.argmax(1).size())
+
+            # print(output.argmax(1))
+            # print(labels)
+            # print(o)
+
+            loss = ClassificationLoss()(output.argmax(1).float(), labels.float())
+
+            optimizer.zero_grad()
+            loss.requires_grad = True
+            loss.backward()
+
+            confusionMatrix = ConfusionMatrix()
+            confusionMatrix.add(output.argmax(1).detach().cpu(), labels.detach().cpu())
+
+            # acc.append(accuracy(o, labels).detach().cpu().numpy())
+            acc.append(confusionMatrix.global_accuracy)
+            iou.append(confusionMatrix.iou)
+
+            train_logger.add_scalar('loss', float(1), global_step=global_step)
+
+            optimizer.step()
+            global_step += 1
+
+        train_logger.add_scalar('accuracy', np.mean(acc), global_step=global_step)
+        train_logger.add_scalar('iou', np.mean(iou), global_step=global_step)
+
+        print('train accuracy: ', np.mean(acc))
+        print('train iou: ', np.mean(iou))
+
+        valid_data, valid_labels = next(iter(valid_dataloader))
+        valid_data, valid_labels = valid_data.to(device), valid_labels.to(device)
+
+        valid_pred = model(valid_data)
+        valid_acc = float(accuracy(valid_pred, valid_labels))
+        print('valid: ', valid_acc)
+
+        valid_logger.add_scalar('accuracy', valid_acc, global_step=global_step)
+
     save_model(model)
 
 
